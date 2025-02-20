3376ae968f596d9968c0ca6e743b8803c44004b5
Kenedy-GrooveDev <kenedylyimo211@gmail.com>
2025-02-21 02:20:08 +0300

    Logo changes and navigation fixes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
-import '../css/Navbar.css'
+import '../css/Navbar.css';
 
 export default function NavBar() {
+  const image = {
+    url: '/flixco-logo.png', // Ensure correct image path
+    width: 100,
+    height: 100
+  }
+
   return (
     <>
       <nav className="navbar">
         <div className="navbar-brand">
-          <Link to='/'>Movie App</Link>
+          <Link to='/'>
+            <img src={image.url} alt="Flixco Logo" style={{ width: image.width, height: image.height }} />
+            <span>Flixco</span>
+          </Link>
         </div>
         <div className="navbar-links">
           <Link to='/' className="nav-link">Home</Link>
--- src/components/pages/Favorites.jsx
+++ src/components/pages/Favorites.jsx
@@ -1,23 +1,24 @@
 import '../../css/Favorites.css';
 import { useMovieContext } from '../../context/MovieContext';
+import MovieCard from '../MovieCard'; // Ensure MovieCard is imported
 
 export default function Favorites() {
-  const {favorites} = useMovieContext();
+  const { favorites = [] } = useMovieContext(); // Ensure favorites is always an array
 
-  if (favorites) {
+  if (favorites.length > 0) {
     return (
-    <div className="movies-grid">
-        {movies.map((movie) => (<MovieCard movie={movie} key={movie.id}/>))}
-    </div>
-    )
+      <div className="movies-grid">
+        {favorites.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    );
   }
 
   return (
-    <>
-      <div className="favorites-empty">
-        <h2>No Favorite Movies Yet</h2>
-        <p>Start adding movies to your favorites and they appear here</p>
-      </div>
-    </>
-  )
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here</p>
+    </div>
+  );
 }
